b8010046c93f3e16ddc7e209c158fad377978c5a
Liz Ardolino <lizardolino@Lizs-MacBook-Pro.local>
2018-10-22 18:12:51 +0100

    player amend test

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -30,18 +30,31 @@ class SlackController < ApplicationController
       )
 
       Rails.logger.info 'R' * 100
+      Rails.logger.info backend_response.inspect
       Rails.logger.info backend_response["game_id"].inspect
       Rails.logger.info backend_response["player"].inspect
       Rails.logger.info 'R' * 100
 
       game_id = backend_response["game_id"]
+      player_exists = backend_response["player"]
 
 
+      if player_exists == 'true'
+        Rails.logger.info "GETTING HERE"
+
+        player_id = backend_response["player_id"]
+        update_player_url = "https://hangman-rails.herokuapp.com/players/#{player_id}"
+        HTTParty.put(update_player_url, 
+        body: {"slack_id"=>"#{slack_id}", "slack_name"=>"#{slack_name}", "game_id"=>"#{game_id}", "response_url"=>"#{response_url}" }.to_json,
+        headers: {"Content-Type" => "application/json"}
+        )
+      else
       create_player_url = "https://hangman-rails.herokuapp.com/players"
       HTTParty.post(create_player_url, 
         body: {"slack_id"=>"#{slack_id}", "slack_name"=>"#{slack_name}", "game_id"=>"#{game_id}", "response_url"=>"#{response_url}" }.to_json,
         headers: {"Content-Type" => "application/json"}
       )
+      end
 
     when "join"
     when "leave"
